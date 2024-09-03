90c03ae8725adc2cdc6431bd862510ee4e237665
hangytongy <145353377+hangytongy@users.noreply.github.com>
2024-09-03 12:01:47 +0800

    Update restart_nillion.sh

--- restart_nillion.sh
+++ restart_nillion.sh
@@ -18,5 +18,5 @@ fi
 
 #somehow in VPS unable to get the latest height, need to create a var and put it in
 #latest_block=$(curl -s "https://testnet.nillion.api.explorers.guru/api/v1/blocks?limit=4&cursor=NTE1MjAzOA==&order_by=asc" | jq '.data[0].height')
-echo "latest block ${latest_block}"
+#echo "latest block ${latest_block}"
 #sleep 10
